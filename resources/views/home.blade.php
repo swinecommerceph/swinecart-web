373e540c7b4cfa1fd369f9b0681d20d80af587b3
Evan Tolorio <evantolorio@gmail.com>
2017-05-25 12:46:50 +0800

    Add 'key features and advantages for Customer' section in homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -119,7 +119,7 @@
     </div>
 
     {{-- Third row --}}
-    <div class="row">
+    <div class="row" style="margin-bottom:0;">
         <div class="col s12 center-align">
             <p class="teal-text text-darken-2">
                 <h4>KEY FEATURES AND ADVANTAGES FOR BREEDERS</h4>
@@ -193,6 +193,83 @@
                     <p class="white-text">
                         Boost your product sales by letting your customers leave feedback and ratings in your breeder profile.
                     </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- Fourth row --}}
+    <div class="row">
+        <div class="col s12 center-align teal darken-4">
+            <p>
+                <h4 class="white-text">KEY FEATURES AND ADVANTAGES FOR CUSTOMERS</h4>
+            </p>
+        </div>
+
+        {{-- First three features --}}
+        <div class="col s12 teal darken-4">
+            <div class="col s4 center" style="margin: 2rem 0 2rem 0;">
+                <div class="col s12">
+                    <i class="ecommerce-icon black-text">n</i>
+                    <h4 class="white-text">TRUSTED SELLERS</h4>
+                    <p class="white-text">
+                        Only farms accredited by Swine Breeders Farm Accreditation Program (SBFAP) can sell their products in SwineCart.
+                    </p>
+                    <p class="white-text">
+                        SBFAP is a program by  Bureau of Animal Industry (BAI) that ensures the availability of quality breeder swines in farms
+                    </p>
+                </div>
+            </div>
+            <div class="col s4 center" style="margin: 2rem 0 2rem 0; border-left: thick solid white; border-right: thick solid white;">
+                <div class="col s12">
+                    <i class="ecommerce-icon black-text">a</i>
+                    <h4 class="white-text">FIND VARIETY OF BREEDERS</h4>
+                    <p class="white-text">
+                        Search for sellers near your area through an integrated map that approximates the location of accredited breeders.
+                    </p>
+                </div>
+            </div>
+            <div class="col s4 center" style="margin: 2rem 0 2rem 0;">
+                <div class="col s12">
+                    <i class="ecommerce-icon black-text">d</i>
+                    <h4 class="white-text">HIGH QUALITY PRODUCTS</h4>
+                    <p class="white-text">
+                        Search for premium quality breeder swine and boar semen and browse through a wide variety of products from accredited breeders. Select products with the aid photos, videos, and complete product specifications
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        {{-- Last two features --}}
+        <div class="container">
+            <div class="col s6" style="margin: 3rem 0 2rem 0; border-right: thick solid #00695c;">
+                <div class="col s12">
+                    <div class="col s12 center">
+                        <i class="ecommerce-icon black-text">c</i>
+                    </div>
+                    <div class="col s12 center">
+                        <h4 class="teal-text text-darken-2">FAST TRANSACTION</h4>
+                    </div>
+                    <div class="col s12 center">
+                        <p class="grey-text text-darken-1">
+                            Communicate with breeders through SwineCart's built-in chat feature. You can also monitor your orders in real-time from reservation to delivery and get updates via SMS and email.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col s6" style="margin: 3rem 0 2rem 0;">
+                <div class="col s12">
+                    <div class="col s12 center">
+                        <i class="ecommerce-icon black-text">v</i>
+                    </div>
+                    <div class="col s12 center">
+                        <h4 class="teal-text text-darken-2">RATING AND FEEDBACK</h4>
+                    </div>
+                    <div class="col s12 center">
+                        <p class="grey-text text-darken-1">
+                            After buying from SwineCart, share your experience with the SwineCart community by posting product and breeder ratings. By sharing your feedback, you can help other customers understand the product and easily find what they need.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
