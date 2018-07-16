81c9c100861e752ab6042d45bae6bbe848f9b8f1
giooooo <gbperalta1@up.edu.ph>
2018-07-16 15:43:45 +0800

    add Message Controller for Breeder

---
+++ app/Http/Controllers/Api/Breeder/MessageController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api\Breeder;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class MessageController extends Controller
+{
+    public function __construct() 
+    {
+        $this->middleware('jwt:auth');
+        $this->middleware('jwt:role:breeder');
+        $this->middleware(function($request, $next) {
+            $this->user = JWTAuth::user();
+            return $next($request);
+        });
+    }
+
+
+    public function countUnread()
+    {   
+        $user_id = $this->user->id;
+
+        $count = Message::where('breeder_id', '=', $user_id)
+				->where('read_at', NULL)
+				->where('direction', 0) //from customer to breeder
+	    		->orderBy('created_at', 'ASC')
+	    		->groupBy('customer_id')
+	    		->get();
+    }
+
+    public function 
+}
