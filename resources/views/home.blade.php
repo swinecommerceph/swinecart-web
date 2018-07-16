c7b8ee61ca6cdd231add8a237cf5982f1055b493
bruce8 <brucealvero@gmail.com>
2018-07-16 16:59:48 +0800

    added footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,22 +28,22 @@
                         <li>
                             <img src="/images/demo/home1.jpg"> <!-- random image -->
                             <div class="caption center-align">
-                                <h3>Welcome to SwineCart!</h3>
-                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+                                {{-- <h3>Welcome to SwineCart!</h3> --}}
+                                <h5 class="light grey-text text-lighten-3"></h5>
                             </div>
                         </li>
                         <li>
                             <img src="/images/demo/home2.jpg"> <!-- random image -->
                             <div class="caption left-align">
-                                <h3>Left Aligned Caption</h3>
-                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+                                {{-- <h3>Left Aligned Caption</h3> --}}
+                                <h5 class="light grey-text text-lighten-3"></h5>
                             </div>
                         </li>
                         <li>
                             <img src="/images/demo/home3.jpg"> <!-- random image -->
                             <div class="caption right-align">
-                                <h3>Right Aligned Caption</h3>
-                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+                                {{-- <h3>Right Aligned Caption</h3> --}}
+                                <h5 class="light grey-text text-lighten-3"></h5>
                             </div>
                         </li>
                     @endforelse
