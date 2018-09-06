b5aab2e6f295b36f06cb0dadd53fdc062e679a92
dom <domlapitan@gmail.com>
2018-09-06 16:34:31 +0800

    change ui of cards in breeder dashboard

--- resources/views/user/breeder/dashboard.blade.php
+++ resources/views/user/breeder/dashboard.blade.php
@@ -22,184 +22,215 @@
 @endsection
 
 @section('content')
+    <br>
     <div class="row">
-        <div class="col s12">
-            <p class="caption" style="font-weight: 700;">
-                Your Dashboard. <br>
-            </p>
-        </div>
+        <h4 class="center-align" style="font-weight: 700;">
+            Overall Performance
+        </h4>
     </div>
 
     <div id="card-status" class="row">
 
-        <p style="color:hsl(0, 0%, 30%); margin-left: 1vw;">Select a frequency to graph:</p>
-        
-        {{-- Charts --}}
-        <div id="charts-container" class="">
-            <div class="col s2">
-                <div class="">
-                    <input class="with-gap" name="frequency" type="radio" id="frequency-monthly" value="monthly" v-model="chosenFrequency" @change="valueChange" />
-                    <label for="frequency-monthly">Monthly</label>
+        <div class="row">
+            {{-- Guide text --}}
+            <p style="color:hsl(0, 0%, 30%); margin-left: 1vw;">Select a frequency to graph:</p>
+            
+            {{-- Charts --}}
+            <div id="charts-container" class="">  
+                {{-- Frequencies --}}
+                <div class="col s2">
+                    <div class="">
+                        <input class="with-gap" name="frequency" type="radio" id="frequency-monthly" value="monthly" v-model="chosenFrequency" @change="valueChange" />
+                        <label for="frequency-monthly">Monthly</label>
+                    </div>
+                    <div class="">
+                        <input class="with-gap" name="frequency" type="radio" id="frequency-weekly" value="weekly" v-model="chosenFrequency" @change="valueChange" />
+                        <label for="frequency-weekly">Weekly</label>
+                    </div>
+                    <div class="">
+                        <input class="with-gap" name="frequency" type="radio" id="frequency-daily" value="daily" v-model="chosenFrequency" @change="valueChange" />
+                        <label for="frequency-daily">Daily</label>
+                    </div>
                 </div>
-                <div class="">
-                    <input class="with-gap" name="frequency" type="radio" id="frequency-weekly" value="weekly" v-model="chosenFrequency" @change="valueChange" />
-                    <label for="frequency-weekly">Weekly</label>
-                </div>
-                <div class="">
-                    <input class="with-gap" name="frequency" type="radio" id="frequency-daily" value="daily" v-model="chosenFrequency" @change="valueChange" />
-                    <label for="frequency-daily">Daily</label>
-                </div>
-            </div>
-            <div class="col s5">
-                <div class="input-field col s5">
-                    <custom-date-from-select v-model="dateFromInput"
-                        :date-accreditation="latestAccreditation"
-                        @date-from-select="dateFromChange"
-                    >
-                    </custom-date-from-select>
-                </div>
-                <div class="input-field col s5">
-                    <custom-date-to-select v-model="dateToInput"
-                        @date-to-select="dateToChange"
-                        v-show="chosenFrequency !== 'weekly'"> </custom-date-to-select>
-                </div>
-                <div class="" style="margin-top:1rem;">
-                    <a class="btn-floating" @click.prevent="retrieveSoldProducts"><i class="material-icons">send</i></a>
-                </div>
-            </div>
 
-            <div class="col s12">
-                <canvas id="barChart"></canvas>
+                {{-- Dates --}}
+                <div class="col s5">
+                    <div class="input-field col s5">
+                        <custom-date-from-select v-model="dateFromInput"
+                            :date-accreditation="latestAccreditation"
+                            @date-from-select="dateFromChange"
+                        >
+                        </custom-date-from-select>
+                    </div>
+                    <div class="input-field col s5">
+                        <custom-date-to-select v-model="dateToInput"
+                            @date-to-select="dateToChange"
+                            v-show="chosenFrequency !== 'weekly'"> </custom-date-to-select>
+                    </div>
+                    <div class="" style="margin-top:1rem;">
+                        <a class="btn-floating" @click.prevent="retrieveSoldProducts"><i class="material-icons">send</i></a>
+                    </div>
+                </div>
+            
+                {{-- Bar Chart--}}
+                <div class="col s12">
+                    <canvas id="barChart"></canvas>
+                </div>
             </div>
-
-            <p><br></p>
         </div>
 
-        <div class="col s12">
-            <br>
-        </div>
-
-        <div id="card-product-status" class="col z-depth-1">
-            <p></p>
+        {{-- Product Status --}}
+        <div id="card-product-status" class="col grey lighten-4">
             <div class="col s12">
-                <h5 class="center-align">
+                <h4 class="center-align">
                     <a href="{{ route('dashboard.productStatus') }}"
                         style="font-weight: 700; color:hsl(0, 0%, 13%);"
                     >
                         Product Status
                     </a>
-                </h5>
+                </h4>
             </div>
 
             {{-- Requested Products --}}
             <div class="col s12 m4">
-                <div class="card">
-                    <div class="card-content white-text blue accent-1">
+                <div class="card hoverable">
+                    <div class="card-content blue lighten-5">
                         <span class="card-title">
                             <a href="{{route('dashboard.productStatus',['status' => 'requested'])}}"
-                                class="white-text"
+                                class="blue-text"
+                                style="font-weight: 700;"
                             >
                                 Requested
                             </a>
                         </span>
                         <h3>@{{ overallRequested }}</h3>
                     </div>
-                    <div class="card-action blue accent-2">
-                        <a class="white-text">Boar: @{{ dashboardStats.requested.boar }} </a>
-                        <a class="white-text">Sow: @{{ dashboardStats.requested.sow }} </a>
-                        <a class="white-text">Gilt: @{{ dashboardStats.requested.gilt }} </a>
-                        <a class="white-text">Semen: @{{ dashboardStats.requested.semen }} </a>
+                    <div class="card-action blue lighten-4">
+                        <a style="color:hsl(0, 0%, 13%);">Boar: @{{ dashboardStats.requested.boar }} </a>
+                        <a style="color:hsl(0, 0%, 13%);">Sow: @{{ dashboardStats.requested.sow }} </a>
+                        <a style="color:hsl(0, 0%, 13%);">Gilt: @{{ dashboardStats.requested.gilt }} </a>
+                        <a style="color:hsl(0, 0%, 13%);">Semen: @{{ dashboardStats.requested.semen }} </a>
                     </div>
                 </div>
             </div>
 
             {{-- Reserved Products --}}
             <div class="col s12 m4">
-                <div class="card">
-                    <div class="card-content grey white-text">
+                <div class="card hoverable">
+                    <div class="card-content blue lighten-5">
                         <span class="card-title">
-                            <a href="{{route('dashboard.productStatus',['status' => 'reserved'])}}" class="white-text">Reserved</a>
+                            <a href="{{route('dashboard.productStatus',['status' => 'reserved'])}}"
+                                class="blue-text"
+                                style="font-weight: 700;"
+                            >
+                                Reserved
+                            </a>
                         </span>
                         <h3>@{{ overallReserved }}</h3>
                     </div>
-                    <div class="card-action grey">
-                        <a class="white-text">Boar: @{{ dashboardStats.reserved.boar }} </a>
-                        <a class="white-text">Sow: @{{ dashboardStats.reserved.sow }} </a>
-                        <a class="white-text">Gilt: @{{ dashboardStats.reserved.gilt }} </a>
-                        <a class="white-text">Semen: @{{ dashboardStats.reserved.semen }} </a>
+                    <div class="card-action blue lighten-4">
+                        <a style="color:hsl(0, 0%, 13%);">Boar: @{{ dashboardStats.reserved.boar }} </a>
+                        <a style="color:hsl(0, 0%, 13%);">Sow: @{{ dashboardStats.reserved.sow }} </a>
+                        <a style="color:hsl(0, 0%, 13%);">Gilt: @{{ dashboardStats.reserved.gilt }} </a>
+                        <a style="color:hsl(0, 0%, 13%);">Semen: @{{ dashboardStats.reserved.semen }} </a>
                     </div>
                 </div>
             </div>
 
             {{-- On Delivery Products --}}
             <div class="col s12 m4">
-                <div class="card">
-                    <div class="card-content pink white-text">
+                <div class="card hoverable">
+                    <div class="card-content blue lighten-5">
                         <span class="card-title">
-                            <a href="{{route('dashboard.productStatus',['status' => 'on_delivery'])}}" class="white-text">On Delivery</a>
+                            <a href="{{route('dashboard.productStatus',['status' => 'on_delivery'])}}"
+                                class="blue-text"
+                                style="font-weight: 700;"
+                            >
+                                On Delivery
+                            </a>
                         </span>
                         <h3>@{{ overallOnDelivery }}</h3>
                     </div>
-                    <div class="card-action pink">
-                        <a class="white-text">Boar: @{{ dashboardStats.on_delivery.boar }} </a>
-                        <a class="white-text">Sow: @{{ dashboardStats.on_delivery.sow }} </a>
-                        <a class="white-text">Gilt: @{{ dashboardStats.on_delivery.gilt }} </a>
-                        <a class="white-text">Semen: @{{ dashboardStats.on_delivery.semen }} </a>
+                    <div class="card-action blue lighten-4">
+                        <a style="color:hsl(0, 0%, 13%);">Boar: @{{ dashboardStats.on_delivery.boar }} </a>
+                        <a style="color:hsl(0, 0%, 13%);">Sow: @{{ dashboardStats.on_delivery.sow }} </a>
+                        <a style="color:hsl(0, 0%, 13%);">Gilt: @{{ dashboardStats.on_delivery.gilt }} </a>
+                        <a style="color:hsl(0, 0%, 13%);">Semen: @{{ dashboardStats.on_delivery.semen }} </a>
                     </div>
                 </div>
             </div>
 
+            <div class="row"></div>
+            <div class="row">
+                <h4 class="center-align" style="font-weight: 700;">Product Management</h4>
+            </div>
+            
 
             {{-- Hidden Products --}}
             <div class="col s12 m4">
-                <div class="card">
-                    <div class="card-content">
+                <div class="card hoverable">
+                    <div class="card-content  blue lighten-5">
                         <span class="card-title">
-                            <a href="{{route('products',['type' => 'all-type', 'status' => 'hidden', 'sort' => 'none'])}}" class="black-text">Hidden</a>
+                            <a href="{{route('products',['type' => 'all-type', 'status' => 'hidden', 'sort' => 'none'])}}"
+                                class="blue-text"
+                                style="font-weight: 700;"
+                            >
+                                Hidden
+                            </a>
                         </span>
                         <h3>@{{ overallHidden }}</h3>
                     </div>
-                    <div class="card-action pink">
-                        <a class="white-text">Boar: @{{ dashboardStats.hidden.boar }} </a>
-                        <a class="white-text">Sow: @{{ dashboardStats.hidden.sow }} </a>
-                        <a class="white-text">Gilt: @{{ dashboardStats.hidden.gilt }} </a>
-                        <a class="white-text">Semen: @{{ dashboardStats.hidden.semen }} </a>
+                    <div class="card-action blue lighten-4">
+                        <a style="color:hsl(0, 0%, 13%);">Boar: @{{ dashboardStats.hidden.boar }} </a>
+                        <a style="color:hsl(0, 0%, 13%);">Sow: @{{ dashboardStats.hidden.sow }} </a>
+                        <a style="color:hsl(0, 0%, 13%);">Gilt: @{{ dashboardStats.hidden.gilt }} </a>
+                        <a style="color:hsl(0, 0%, 13%);">Semen: @{{ dashboardStats.hidden.semen }} </a>
                     </div>
                 </div>
             </div>
 
             {{-- Displayed Products --}}
             <div class="col s12 m4">
-                <div class="card">
-                    <div class="card-content">
+                <div class="card hoverable">
+                    <div class="card-content blue lighten-5">
                         <span class="card-title">
-                            <a href="{{route('products',['type' => 'all-type', 'status' => 'displayed', 'sort' => 'none'])}}" class="black-text">Displayed</a>
+                            <a href="{{route('products',['type' => 'all-type', 'status' => 'displayed', 'sort' => 'none'])}}"
+                                class="blue-text"
+                                style="font-weight: 700;"
+                            >
+                                Displayed
+                            </a>
                         </span>
                         <h3>@{{ overallDisplayed }}</h3>
                     </div>
-                    <div class="card-action teal">
-                        <a class="white-text">Boar: @{{ dashboardStats.displayed.boar + dashboardStats.requested.boar }} </a>
-                        <a class="white-text">Sow: @{{ dashboardStats.displayed.sow + dashboardStats.requested.sow }} </a>
-                        <a class="white-text">Gilt: @{{ dashboardStats.displayed.gilt + dashboardStats.requested.gilt }} </a>
-                        <a class="white-text">Semen: @{{ dashboardStats.displayed.semen + dashboardStats.requested.semen }} </a>
+                    <div class="card-action blue lighten-4">
+                        <a style="color:hsl(0, 0%, 13%);">Boar: @{{ dashboardStats.displayed.boar + dashboardStats.requested.boar }} </a>
+                        <a style="color:hsl(0, 0%, 13%);">Sow: @{{ dashboardStats.displayed.sow + dashboardStats.requested.sow }} </a>
+                        <a style="color:hsl(0, 0%, 13%);">Gilt: @{{ dashboardStats.displayed.gilt + dashboardStats.requested.gilt }} </a>
+                        <a style="color:hsl(0, 0%, 13%);">Semen: @{{ dashboardStats.displayed.semen + dashboardStats.requested.semen }} </a>
                     </div>
                 </div>
             </div>
 
             {{-- Total Products Available --}}
             <div class="col s12 m4">
-                <div class="card">
-                    <div class="card-content">
+                <div class="card hoverable">
+                    <div class="card-content blue lighten-5">
                         <span class="card-title">
-                            <a href="{{route('products',['type' => 'all-type', 'status' => 'all-status', 'sort' => 'none'])}}" class="black-text">Available Products</a>
+                            <a href="{{route('products',['type' => 'all-type', 'status' => 'all-status', 'sort' => 'none'])}}"
+                                    class="blue-text"
+                                    style="font-weight: 700;"
+                                >
+                                    Available Products
+                                </a>
                         </span>
                         <h3>@{{ overallProductsAvailable }}</h3>
                     </div>
-                    <div class="card-action grey">
-                        <a class="white-text">Boar: @{{ dashboardStats.requested.boar + dashboardStats.displayed.boar + dashboardStats.hidden.boar }} </a>
-                        <a class="white-text">Sow: @{{ dashboardStats.requested.sow + dashboardStats.displayed.sow + dashboardStats.hidden.sow }} </a>
-                        <a class="white-text">Gilt: @{{ dashboardStats.requested.gilt + dashboardStats.displayed.gilt + dashboardStats.hidden.gilt }} </a>
-                        <a class="white-text">Semen: @{{ dashboardStats.requested.semen + dashboardStats.displayed.semen + dashboardStats.hidden.semen }} </a>
+                    <div class="card-action blue lighten-4">
+                        <a style="color:hsl(0, 0%, 13%);">Boar: @{{ dashboardStats.requested.boar + dashboardStats.displayed.boar + dashboardStats.hidden.boar }} </a>
+                        <a style="color:hsl(0, 0%, 13%);">Sow: @{{ dashboardStats.requested.sow + dashboardStats.displayed.sow + dashboardStats.hidden.sow }} </a>
+                        <a style="color:hsl(0, 0%, 13%);">Gilt: @{{ dashboardStats.requested.gilt + dashboardStats.displayed.gilt + dashboardStats.hidden.gilt }} </a>
+                        <a style="color:hsl(0, 0%, 13%);">Semen: @{{ dashboardStats.requested.semen + dashboardStats.displayed.semen + dashboardStats.hidden.semen }} </a>
                     </div>
                 </div>
             </div>
