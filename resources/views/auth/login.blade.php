5f616672f0922b34ace1c2e916100d77f6c00b36
dom <domlapitan@gmail.com>
2018-12-06 11:11:46 +0800

    Resolve login error

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,7 +15,6 @@
 			<div class="card-panel" style="border: solid 1px #E1E4EC !important; box-shadow: 0px 0px !important; border-radius: 3px !important;">
 				<div class="row s12">
 					<p class="left-align" style="font-size: 1.125rem; line-height: 1.2; color: hsl(0, 0%, 45%);"> Login to your account</p>
-
 					{{-- Display Validation Errors --}}
 					@include('common._errors')
 
@@ -24,42 +23,43 @@
 						{!! csrf_field() !!}
 
 						{{-- E-Mail Address --}}
-						<div class="row input-field">
-							<label for="email" style="font-weight: 700; color: hsl(0, 0%, 45%); font-size: 0.9rem;">E-mail address</label>
-							<input class="login" type="email" id="email" name="email" value="{{ old('email') }}" autofocus placeholder="  Enter email" required>
-						</div>
+            <div class="row input-field">
+              <label for="email" style="font-weight: 700; color: hsl(0, 0%, 45%); font-size: 0.9rem;">E-mail address</label>
+              <input class="login" type="email" id="email" name="email" value="{{ old('email') }}" autofocus placeholder="  Enter email" required>
+            </div>
 
 						{{-- Password --}}
-						<div class="row input-field">
-							<span for="password" style="font-weight: 700; color: hsl(0, 0%, 45%); font-size: 0.9rem;">Password</span>
-							<span style="display: inline-block; width: 4rem;"></span>
-							<span><a style="font-size: 0.9rem;" href="/password/reset"> I forgot my password </a></span>	
-							<input class="login" type="password" id="password" name="password" placeholder="  Password" required>
-						</div>
+            <div class="row input-field">
+              <span for="password" style="font-weight: 700; color: hsl(0, 0%, 45%); font-size: 0.9rem;">Password</span>
+              <span style="display: inline-block; width: 4rem;"></span>
+              <span><a style="font-size: 0.9rem;" href="/password/reset"> I forgot my password </a></span>  
+              <input class="login" type="password" id="password" name="password" placeholder="  Password" required>
+            </div>
 
 						{{-- Login Button --}}
 						<div class="row">
-								<a type="submit" class="btn waves-effect waves-light col s12 teal darken-3">Login with Email</a>
-						</div>
+                <button type="submit" class="btn waves-effect waves-light col s12 teal darken-3">Login with Email</button>
+            </div>
+
 					</form>
 
 					{{-- Social Login --}}
-					<p class="center-align"> OR </p>
+          <p class="center-align"> OR </p>
 
-					{{-- Facebook Button --}}
-					<a href="/login/facebook" class="waves-effect waves-light col s12 btn facebook">
-						<i style="border-right: solid 1px rgba(0, 0, 0, 0.2); margin-right: 1rem; padding: 0 1.3rem;" class="fa fa-facebook"></i>
-					  Login with Facebook
-					</a>
+          {{-- Facebook Button --}}
+          <a href="/login/facebook" class="waves-effect waves-light col s12 btn facebook">
+            <i style="border-right: solid 1px rgba(0, 0, 0, 0.2); margin-right: 1rem; padding: 0 1.3rem;" class="fa fa-facebook"></i>
+            Login with Facebook
+          </a>
 
-					<br><br>
+          <br><br>
 
-					{{-- Google Button --}}
-					<a href="/login/google" class="waves-effect waves-light red col s12 btn google">
-						<i style="border-right: solid 1px rgba(0, 0, 0, 0.2); margin-right: 1rem; padding-right: 1rem; padding-left: 0.1rem;" class="fa fa-google"></i>
-					  Login with Google
-					</a>
-					
+          {{-- Google Button --}}
+          <a href="/login/google" class="waves-effect waves-light red col s12 btn google">
+            <i style="border-right: solid 1px rgba(0, 0, 0, 0.2); margin-right: 1rem; padding-right: 1rem; padding-left: 0.1rem;" class="fa fa-google"></i>
+            Login with Google
+          </a>
+                   
 				</div>
 			</div>
 		</div>
