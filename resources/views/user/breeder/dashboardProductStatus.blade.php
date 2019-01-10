97de9e905a2522e8cc306ddf722db0a3ad07cf8a
dom <domlapitan@gmail.com>
2019-01-10 11:43:22 +0800

    Remove Dashboard in Breadcrumbs in Breeder Product Inventory and Status Page

--- resources/views/user/breeder/dashboardProductStatus.blade.php
+++ resources/views/user/breeder/dashboardProductStatus.blade.php
@@ -18,7 +18,6 @@
 
 @section('breadcrumb')
     <a href="{{ route('home_path') }}" class="breadcrumb">Home</a>
-    <a href="{{ route('dashboard') }}" class="breadcrumb">Dashboard</a>
     <a href="#!" class="breadcrumb">Product Inventory & Status</a>
 @endsection
 
