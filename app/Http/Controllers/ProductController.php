47cb8944853a501fd115268a3abe9f49795bf434
Dominic Lapitan <domlapitan@gmail.com>
2019-04-26 14:15:21 +0800

    Update query in Show Products in Breeder

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -99,7 +99,18 @@ class ProductController extends Controller
     public function showProducts(Request $request)
     {
         $breeder = $this->user->userable;
-        $products = $breeder->products()->whereIn('status',['hidden','displayed','requested'])->where('quantity','<>',0);
+        $products = $breeder->products()->whereIn('status',['hidden','displayed','requested'])
+          ->where(
+            [
+              ['is_unique', '=', 1], ['quantity', '!=', 0] 
+            ]
+          )
+          ->orWhere(
+            [
+              ['is_unique', '=', 0], ['quantity', '>=', -1],
+            ]
+          )
+        ;
 
         // Check filters
         if($request->type && $request->type != 'all-type') $products = $products->where('type',$request->type);
