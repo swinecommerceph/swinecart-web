ce2ce126c22bbcaa282b641031c96e296a0ffbf8
Dominic Lapitan <domlapitan@gmail.com>
2019-06-25 17:24:46 +0800

    Fix proper query in Show Products in Breeder

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -99,18 +99,28 @@ class ProductController extends Controller
     public function showProducts(Request $request)
     {
         $breeder = $this->user->userable;
-        $products = $breeder->products()->whereIn('status',['hidden','displayed','requested'])
-          ->where(
-            [
-              ['is_unique', '=', 1], ['quantity', '!=', 0] 
-            ]
-          )
-          ->orWhere(
-            [
-              ['is_unique', '=', 0], ['quantity', '>=', -1],
-            ]
-          )
-        ;
+        //dd($breeder->products()->get());
+        /* $products = $breeder->products()
+                    ->whereIn('status',['hidden','displayed','requested']); */
+                      
+        /* $products = $products->where([
+                          ['is_unique', '=', 1],
+                          ['quantity', '>', 0] 
+                      ])
+                      ->orWhere([
+                          ['is_unique', '=', 0],
+                          ['quantity', '>=', -1]
+                      ]); */
+        
+        
+                    
+        $products = $breeder->products()
+                    ->whereIn('status',['hidden','displayed','requested'])
+                    ->where('quantity','<>',0)
+                    ->orWhere([
+                      ['is_unique', '=', '0'],
+                      ['quantity', '=', '0']
+                    ]);
 
         // Check filters
         if($request->type && $request->type != 'all-type') $products = $products->where('type',$request->type);
@@ -146,6 +156,8 @@ class ProductController extends Controller
             $product->breed = $this->transformBreedSyntax(Breed::find($product->breed_id)->name);
             $product->other_details = $this->transformOtherDetailsSyntax($product->other_details);
         }
+
+        /* dd($products); */
 
         return view('user.breeder.showProducts', compact('products', 'farms', 'filters', 'urlFilters'));
     }
