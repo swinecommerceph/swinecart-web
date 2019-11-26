942b72e4a2a7d20e99a974025f46da4ac6ce5576
dilapitan <domlapitan@gmail.com>
2019-11-26 16:45:30 +0800

    Finish the new UI for breeder profile seen by Customer

---
+++ resources/views/user/customer/_viewBreederProductsOnly.blade.php
@@ -0,0 +1,189 @@
+<div class="col s12">
+
+  <div class="row">
+    <!-- Product 1 -->
+    <div class="col s12 m6">
+
+      <div class="card hoverable">
+
+        <div class="card-image" style="padding: 20px 20px;">
+          <img style="width: 25vw; height: 15vh;" class="activator" src="https://source.unsplash.com/random/300x200" alt="">
+        </div>
+
+        <div class="card-content" style="background: hsl(0, 0%, 97%);">
+          <div class="row">
+
+            <div class="col s10">
+              <span class="card-title truncate" style="color: hsl(0, 0%, 13%); font-weight: 700;">
+                1572
+              </span>
+            </div>
+
+            <div class="col s1">
+              <span>
+                <i class="activator material-icons right" style="cursor: pointer;">more_vert</i>
+              </span>
+            </div>
+
+            <div class="row">
+              <div class="col s9">
+
+                <span style="color: hsl(0, 0%, 13%); font-weight: 550;">
+                  Boar - Duroc
+                </span> <br>
+                  <span style="color: hsl(0, 0%, 45%);">Age: 99 days old</span>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+        <!-- card content -->
+        <div class="card-reveal">
+          <div class="row">
+            <div class="col s10">
+              <p>Other Info</p>
+            </div>
+            <div class="col s1">
+              <span><i class="card-title material-icons right" style="cursor: pointer;">close</i></span>  
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+    
+    <!-- Product 2 -->
+    <div class="col s12 m6">
+
+      <div class="card hoverable">
+
+        <div class="card-image" style="padding: 20px 20px;">
+          <img style="width: 25vw; height: 15vh;" class="activator" src=" https://source.unsplash.com/random/300x200" alt="">
+        </div>
+
+        <div class="card-content" style="background: hsl(0, 0%, 97%);">
+          <div class="row">
+
+            <div class="col s10">
+              <span class="card-title truncate" style="color: hsl(0, 0%, 13%); font-weight: 700;">
+                1672
+              </span>
+            </div>
+
+            <div class="col s1">
+              <span>
+                <i class="activator material-icons right" style="cursor: pointer;">more_vert</i>
+              </span>
+            </div>
+
+            <div class="row">
+              
+              <div class="col s9">
+
+                <span style="color: hsl(0, 0%, 13%); font-weight: 550;">
+                  Sow - Largewhite
+                </span> <br>
+                  <span style="color: hsl(0, 0%, 45%);">Age: 150 days old</span>
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+    <!-- Product 3 -->
+    <div class="col s12 m6">
+
+      <div class="card hoverable">
+
+        <div class="card-image" style="padding: 20px 20px;">
+          <img style="width: 25vw; height: 15vh;" class="activator" src=" https://source.unsplash.com/random/300x200" alt="">
+        </div>
+
+        <div class="card-content" style="background: hsl(0, 0%, 97%);">
+          <div class="row">
+
+            <div class="col s10">
+              <span class="card-title truncate" style="color: hsl(0, 0%, 13%); font-weight: 700;">
+                1111
+              </span>
+            </div>
+
+            <div class="col s1">
+              <span>
+                <i class="activator material-icons right" style="cursor: pointer;">more_vert</i>
+              </span>
+            </div>
+
+            <div class="row">
+              
+              <div class="col s9">
+
+                <span style="color: hsl(0, 0%, 13%); font-weight: 550;">
+                  Semen - Duroc
+                </span> <br>
+                  <span style="color: hsl(0, 0%, 45%);">Age: 67 days old</span>
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+    <!-- Product 4 -->
+    <div class="col s12 m6">
+
+      <div class="card hoverable">
+
+        <div class="card-image" style="padding: 20px 20px;">
+          <img style="width: 25vw; height: 15vh;" class="activator" src=" https://source.unsplash.com/random/300x200" alt="">
+        </div>
+
+        <div class="card-content" style="background: hsl(0, 0%, 97%);">
+          <div class="row">
+
+            <div class="col s10">
+              <span class="card-title truncate" style="color: hsl(0, 0%, 13%); font-weight: 700;">
+                F1
+              </span>
+            </div>
+
+            <div class="col s1">
+              <span>
+                <i class="activator material-icons right" style="cursor: pointer;">more_vert</i>
+              </span>
+            </div>
+
+            <div class="row">
+              
+              <div class="col s9">
+
+                <span style="color: hsl(0, 0%, 13%); font-weight: 550;">
+                  Gilt - Largewhite
+                </span> <br>
+                  <span style="color: hsl(0, 0%, 45%);">Age: 222 days old</span>
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+    
+
+  </div>
+
+</div>
--- resources/views/user/customer/viewBreederProfile.blade.php
+++ resources/views/user/customer/viewBreederProfile.blade.php
@@ -1,207 +1,64 @@
 {{--
-    Display Breeder's profile
+  Display Breeder's profile
 --}}
 
 @extends('user.customer.home')
 
 @section('title')
-    | {{ $breeder->name }}
+  | {{ $breeder->name }}
 @endsection
 
 @section('pageId')
-    id="page-view-breeder-profile"
+  id="page-view-breeder-profile"
 @endsection
 
 @section('breadcrumbTitle')
-    Breeder's Profile
+  Breeder's Profile
 @endsection
 
 @section('breadcrumb')
-    <a href="{{ route('home_path') }}" class="breadcrumb">Home</a>
-    <a href="{{ route('products.view') }}" class="breadcrumb">Products</a>
-    <a href="#!" class="breadcrumb">{{ $breeder->name }}</a>
+  <a href="{{ route('home_path') }}" class="breadcrumb">Home</a>
+  <a href="{{ route('products.view') }}" class="breadcrumb">Products</a>
+  <a href="#!" class="breadcrumb">{{ $breeder->name }}</a>
 @endsection
 
 @section('content')
-    <div class="row container">
-        <div class="collection with-header">
-            <div class="teal darken-2 white-text collection-header">
-                {{-- First Row --}}
-                <h4 style="font-weight: 700;">
-                    {{ $breeder->name }}
-                    <img class="secondary-content" src="{{ $breeder->logoImage }}" style="width: 8vw; height:13vh;" alt="" />
-                </h4>
+  <div class="row container">
 
-                <span class="grey-text text-lighten-4">
-                    {{ $breeder->officeAddress_addressLine1 }},
-                    {{ $breeder->officeAddress_addressLine2 }},
-                    {{ $breeder->officeAddress_province }},
-                    {{ $breeder->officeAddress_zipCode }}
-                </span>
-            </div>
-
-            {{-- Second Row --}}
-
-            {{-- Breeder Details --}}
-            <div class="white row">
-                <div class="col s0.5"></div>
-                <div class="col s6">
-                    <div class="row s12"></div>
-                    <div class="row s12">
-                        <table class="highlight" style="width: 100%;">
-                            <tbody>
-                                <tr>
-                                    <td style="color: hsl(0, 0%, 13%); padding: 5px;">
-                                         Website:
-                                    </td>
-                                    <td style="color: hsl(0, 0%, 13%); padding: 5px;">
-                                          {{ $breeder->website }}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td style="color: hsl(0, 0%, 13%); padding: 5px;">
-                                        Produce:
-                                    </td>
-                                    <td style="color: hsl(0, 0%, 13%); padding: 5px;">
-                                        {{ $breeder->produce }}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td style="color: hsl(0, 0%, 13%); padding: 5px;">
-                                        Office Landline:
-                                    </td>
-                                    <td style="color: hsl(0, 0%, 13%); padding: 5px;">
-                                        {{ $breeder->office_landline }}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td style="color: hsl(0, 0%, 13%); padding: 5px;">
-                                        Mobile Landline:
-                                    </td>
-                                    <td style="color: hsl(0, 0%, 13%); padding: 5px;">
-                                        {{ $breeder->office_mobile }}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td style="color: hsl(0, 0%, 13%); padding: 5px;">
-                                        Contact Person:
-                                    </td>
-                                    <td style="color: hsl(0, 0%, 13%); padding: 5px;">
-                                        {{ $breeder->contactPerson_name }}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td style="color: hsl(0, 0%, 13%); padding: 5px;">
-                                        Contact Person's Mobile:
-                                    </td>
-                                    <td style="color: hsl(0, 0%, 13%); padding: 5px;">
-                                        {{ $breeder->contactPerson_mobile }}
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-                <div class="col s5"></div>
-            </div>
-            
-            {{--
-            <li class="collection-item row">
-                <span class="col s3"> Website </span>
-                <span class="col s9">{{ $breeder->website }}</span>
-            </li>
-            <li class="collection-item row">
-                <span class="col s3"> Produce </span>
-                <span class="col s9">{{ $breeder->produce }}</span>
-            </li>
-            <li class="collection-item row">
-                <span class="col s3"> Office Landline </span>
-                <span class="col s9">{{ $breeder->office_landline }}</span>
-                <span class="col s3"> Office Mobile </span>
-                <span class="col s9">{{ $breeder->office_mobile }}</span>
-            </li>
-            <li class="collection-item row">
-                <span class="col s3"> Contact Person </span>
-                <span class="col s9">{{ $breeder->contactPerson_name }}</span>
-                <span class="col s3"> Contact Person Mobile </span>
-                <span class="col s9">{{ $breeder->contactPerson_mobile }}</span>
-            </li>
-            --}} 
-            
-            <div class="teal darken-2 white-text collection-header">
-                <h4 style="font-weight: 700;">Farms</h4>
-            </div>
-
-            <div class="collection-item row">
-                @foreach ($breeder->farms as $farm)    
-                        <span class="s8">
-                            <h5 style="font-weight: 600;">Farm {{ $loop->index + 1 }}</h5>
-                            <table class="highlight" style="width:100%;">
-                                <tr>
-                                    <td style="color: hsl(0, 0%, 13%); padding: 5px;">
-                                        Farm Name: 
-                                    </td>
-                                    <td style="color: hsl(0, 0%, 13%); padding: 5px;">
-                                        {{ $farm->name }}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td style="color: hsl(0, 0%, 13%); padding: 5px;">
-                                        Address: 
-                                    </td>
-                                    <td style="color: hsl(0, 0%, 13%); padding: 5px;">
-                                        {{ $farm->addressLine1 }}, {{ $farm->addressLine2 }}, {{ $farm->province }}, {{ $farm->zipCode }}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td style="color: hsl(0, 0%, 13%); padding: 5px;">
-                                        Accreditation Date:
-                                    </td>
-                                    <td style="color: hsl(0, 0%, 13%); padding: 5px;">
-                                        {{ date_format(date_create($farm->accreditation_date), 'F Y') }}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td style="color: hsl(0, 0%, 13%); padding: 5px;">
-                                        Farm Type:
-                                    </td>
-                                    <td style="color: hsl(0, 0%, 13%); padding: 5px;">
-                                        {{ $farm->farmType }}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td style="color: hsl(0, 0%, 13%); padding: 5px;">
-                                        Farm Landline:
-                                    </td>
-                                    <td style="color: hsl(0, 0%, 13%); padding: 5px;">
-                                        {{ $farm->landline }}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td style="color: hsl(0, 0%, 13%); padding: 5px;">
-                                        Farm Mobile:
-                                    </td>
-                                    <td style="color: hsl(0, 0%, 13%); padding: 5px;">
-                                        {{ $farm->mobile }}
-                                    </td>
-                                </tr>
-                            </table>
-                            
-                            {{--
-                            <span>Farm Name: {{ $farm->name }}</span> <br>
-                            <span>Address: {{ $farm->addressLine1 }}, {{ $farm->addressLine2 }}, {{ $farm->province }}, {{ $farm->zipCode }}</span>
-                            <span>Accreditation Date: {{ date_format(date_create($farm->accreditation_date), 'F Y') }}</span> <br>
-                            <span>Farm Type: {{ $farm->farmType }}</span> <br>
-                            <span>Farm Landline: {{ $farm->landline }}</span> <br>
-                            <span>Farm Mobile: {{ $farm->mobile }}</span> <br>
-                            --}}
-                        </span>
-                @endforeach
-            </div>
-        </div>
+    <!-- Tabs -->
+    <div class="col s12">
+      <ul class="tabs z-depth-1">
+        <li id="breeder-products-tab" class="tab col s6">
+          <a href="#breeder-products">Breeder's Products</a>
+        </li>
+        <li id="breeder-profile-tab" class="tab col s6">
+          <a href="#breeder-profile">Breeder's Profile</a>
+        </li>
+      </ul>
     </div>
+
+    <!-- Breeder Products Tab-->
+    <div class="col s12">
+      <div id="breeder-products" class="card-panel">
+        <div class="row">
+          @include('user.customer._viewBreederProductsOnly')
+        </div>
+      </div>
+    </div>
+
+    <!-- Breeder Profile Tab -->
+    <div class="col s12">
+      <div id="breeder-profile" class="card-panel">
+        <div class="row">
+          @include('user.customer._viewBreederProfileOnly')
+        </div>
+      </div>
+    </div>
+
+  </div>
+
 @endsection
 
 @section('customScript')
-
+  <script src="{{ elixir('/js/customer/viewBreederProfile.js') }}"></script>
 @endsection
