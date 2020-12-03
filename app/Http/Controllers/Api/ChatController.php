9ec1dbd51c3d8d88091ed52ce5395c61c0910271
giooooo <gbperalta1@up.edu.ph>
2020-12-03 19:39:39 +0800

    add getChats function

--- app/Http/Controllers/Api/ChatController.php
+++ app/Http/Controllers/Api/ChatController.php
@@ -25,6 +25,54 @@ class ChatController extends Controller
 
     public function getChats(Request $request)
     {
+        $user_id = $this->user->id;
+        $user_str = $this->accountType === 'Breeder'
+            ? 'breeder_id'
+            : 'customer_id';
+        $other_user_str = $user_str === 'breeder_id'
+            ? 'customer_id'
+            : 'breeder_id';
+
+        $threads = Message::where($user_str, '=', $user_id)
+	    		->orderBy('id', 'DESC')
+                ->get()
+                ->unique($other_user_str)
+                ->values()
+                ->forPage($request->page, $request->limit)
+                ->map(function ($item) use ($other_user_str) {
+
+                    $thread = [];
+
+                    $user = User::find($item->{$other_user_str});
+
+                    $thread['user'] = [
+                        'id' => $user->id,
+                        'name' => $user->name
+                    ];
+
+                    $thread['message'] = [
+                        'id' => $item->id,
+                        'direction' => $item->direction,
+                        'content' => $item->message,
+                        'readAt' => $item->read_at,
+                        'createdAt' => $item->created_at->toDateTimeString(),
+                        'fromId' => $item->direction === 1
+                            ? $item->breeder_id : $item->customer_id,
+                        'toId' => $item->direction === 1
+                            ? $item->customer_id : $item->breeder_id,
+                    ];
+
+                    return $thread;
+                })
+                ->values()
+                ->all();
+
+        return response()->json([
+            'data' => [
+                'threads' => $threads,
+            ]
+        ], 200);
+
         return response()->json([
             'data' => 'hello'
         ], 200);
@@ -65,8 +113,7 @@ class ChatController extends Controller
 
         return response()->json([
             'data' => [
-                'count' => $result->count(),
-                'hasNext' => $result->hasMorePages(),
+                'hasNextPage' => $result->hasMorePages(),
                 'messages' => $messages,
             ]
         ], 200);
