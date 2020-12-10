c8f427da09c0ac3b910f568ffc9ce10e8bee5ddf
giooooo <gbperalta1@up.edu.ph>
2020-12-11 01:26:22 +0800

    refactor ShopController remove unused functions add joins to optimize query

--- app/Http/Controllers/Api/Customer/ShopController.php
+++ app/Http/Controllers/Api/Customer/ShopController.php
@@ -4,37 +4,19 @@ namespace App\Http\Controllers\Api\Customer;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Tymon\JWTAuth\Exceptions\JWTException;
-use Carbon\Carbon;
-use Illuminate\Support\Collection;
 
-use App\Http\Requests\ChangePasswordRequest;
-use App\Http\Requests\BreederPersonalProfileRequest;
-use App\Http\Requests\BreederFarmProfileRequest;
-use App\Http\Requests\ProductRequest;
-
-use App\Models\Image;
-use App\Models\User;
 use App\Models\Breeder;
 use App\Models\Breed;
-use App\Models\Customer;
-use App\Models\FarmAddress;
 use App\Models\Product;
 
 use App\Repositories\ProductRepository;
 use App\Repositories\CustomHelpers;
 
-use Auth;
 use Response;
-use Validator;
 use JWTAuth;
-use Mail;
-use Storage;
-use Config;
 
 class ShopController extends Controller
 {
-    
     use CustomHelpers {
         transformBreedSyntax as private;
         transformDateSyntax as private;
@@ -42,9 +24,7 @@ class ShopController extends Controller
         computeAge as private;
     }
 
-    const IMG_PATH = '/images/';
-
-    public function __construct() 
+    public function __construct()
     {
         $this->middleware('jwt:auth');
         $this->middleware('jwt.role:customer');
@@ -54,33 +34,37 @@ class ShopController extends Controller
         });
     }
 
-    private function getBreedIds($breedParameter)
-    {
-        $tempBreedIds = [];
-        foreach (explode(',', $breedParameter) as $breedName) {
-            if($breedName == 'crossbreed') {
-                // Get all breed ids that contain '+' in their breed name
-                $crossbreeds = Breed::where('name','like','%+%')->get();
-                foreach ($crossbreeds as $crossbreed) {
-                    array_push($tempBreedIds, $crossbreed->id);
-                }
-                continue;
-            }
-            else {
-                $breedInstance = Breed::where('name', $breedName)->get()->first();
-                array_push($tempBreedIds, $breedInstance && $breedInstance->id);
-            }
-            
-        }
-
-        return $tempBreedIds;
-    }
-
     public function getProducts(Request $request, ProductRepository $repository)
-    {   
-        $products = Product::whereIn('status', ['displayed', 'requested'])
-            ->with('breed', 'breeder.user', 'primaryImage')
-            ->where('quantity', '<>', 0);
+    {
+        $products = Product::with(
+                'breed',
+                'breeder.user',
+                'primaryImage'
+            )
+            ->join('farm_addresses', function ($join) {
+                $join
+                    ->on(
+                        'products.farm_from_id',
+                        '=',
+                        'farm_addresses.id'
+                    )
+                    ->where('accreditation_status', 'active');
+            })
+            ->select(
+                'products.*',
+                'farm_addresses.name as fa.name',
+                'farm_addresses.id as fa.id'
+            )
+            ->whereIn('status', ['displayed', 'requested'])
+            ->where(function ($query) {
+                $query
+                    ->where('quantity', '<>', 0)
+                    ->orWhere([
+                        ['is_unique', '=', 0],
+                        ['quantity', '=', 0]
+                    ]);
+            });
+
 
         // Search
         if($request->input('q')) {
@@ -118,46 +102,44 @@ class ShopController extends Controller
             $products = $products->orderBy($part[0], $part[1]);
         }
         else {
-            $products = $products->orderBy('id', 'desc');
+            $products = $products->orderBy('id', 'DESC');
         }
 
         // Paginate and Transform Product
-        $products = $products
-            ->paginate($request->limit)
-            ->reduce(function($array, $product) {
-                if($product->farmFrom->accreditation_status == 'active') {
-                    $breeder = $product->breeder;
-                    $breed = $product->breed;
+        $products = $products->paginate($request->limit);
+        $formatted = $products->map(function($item) {
 
-                    $p = [];
+            $product = [];
 
-                    $p['id'] = $product->id;
-                    $p['name'] = $product->name;
-                    $p['type'] = $product->type;
-                    $p['age'] = $product->birthdate === '0000-00-00'
-                        ? null
-                        : $this->computeAge($product->birthdate);
-                    $p['breed'] = $this->transformBreedSyntax($breed->name);
-                    $p['breederName'] = $breeder->user->name;
-                    $p['farmLocation'] = $product->farmFrom->province;
-                    $p['imageUrl'] = route('serveImage',
-                        [
-                            'size' => 'medium',
-                            'filename' => $product->primaryImage->name
-                        ]
-                    );
+            $breeder = $item->breeder;
+            $breed = $item->breed;
 
-                    $array->push($p);
-                }
+            $product['id'] = $item->id;
+            $product['name'] = $item->name;
+            $product['type'] = $item->type;
+            $product['quantity'] = $item->quantity;
+            $product['isUnique'] = $item->is_unique === 1;
+            $product['age'] = $item->birthdate === '0000-00-00'
+                ? null
+                : $this->computeAge($item->birthdate);
+            $product['breed'] = $this->transformBreedSyntax($breed->name);
+            $product['breederName'] = $breeder->user->name;
+            $product['imageUrl'] = route('serveImage',
+                [
+                    'size' => 'small',
+                    'filename' => $item->primaryImage->name
+                ]
+            );
 
-                return $array;
-            }, collect([]));
+            return $product;
+        });
 
         return response()->json([
             'data' => [
-                'products' => $products,
+                'hasNextPage' => $products->hasMorePages(),
+                'products' => $formatted,
             ]
-        ], 200);
+        ]);
     }
 
     public function getFilterOptions(Request $request)
@@ -173,7 +155,7 @@ class ShopController extends Controller
                 $breed['name'] = ucwords($item->name);
                 return $breed;
             });
-        
+
         $breeders = Breeder::with('users')
             ->get(['id'])
             ->map(function ($item) {
@@ -191,42 +173,4 @@ class ShopController extends Controller
         ], 200);
     }
 
-
-    public function getBreeds(Request $request)
-    {
-        $breeds = Breed::where('name','not like', '%+%')
-            ->where('name','not like', '')
-            ->orderBy('name','asc')
-            ->get(['id', 'name'])
-            ->map(function ($item) {
-                $breed = [];
-                $breed['id'] = $item->id;
-                $breed['name'] = ucwords($item->name);
-                return $breed;
-            });
-
-        return response()->json([
-            'data' => [
-                'breeds' => $breeds
-            ]
-        ], 200);
-    }
-
-    public function getBreeders(Request $request)
-    {
-        $breeders = Breeder::with('users')
-            ->get(['id'])
-            ->map(function ($item) {
-                $breeder = [];
-                $breeder['id'] = $item->id;
-                $breeder['name'] = ucwords($item->users()->first()->name);
-                return $breeder;
-            });
-
-        return response()->json([
-            'data' => [
-                'breeders' => $breeders
-            ]
-        ], 200);
-    }
 }
